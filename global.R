7a2ad016663d4cf8084eeaf442a849806c67519f
kdaily <kdaily@users.noreply.github.com>
2016-01-14 12:15:20 -0800

    cleanup global

--- global.R
+++ global.R
@@ -1,6 +1,3 @@
-library(synapseClient)
-synapseLogin()
-
 library(data.table)
 library(plyr)
 library(dplyr)
@@ -12,21 +9,3 @@ nodeProperties <- data.frame(group=c("mrna", "nontf_mrna",
                                      "mirna", "methyl"),
                              color=c("#888888", "#008800", "#880000", "#000088"),
                              shape=c("ellipse", "ellipse", "square", "square"))
-
-# source("tf_mirna.R")
-# source("tf_methyl.R")
-# source("nontf_mirna.R")
-# 
-# diffStates <- unique(c(diffStates.tf_mirna, 
-#                        diffStates.tf_methyl,
-#                        diffStates.nontf_mirna))
-# 
-# nodeData <- rbind(nodeData.tf_mirna, 
-#                   nodeData.tf_methyl,
-#                   nodeData.nontf_mirna)
-# 
-# edgeData <- rbind(edgeData.tf_mirna, 
-#                   edgeData.tf_methyl,
-#                   edgeData.nontf_mirna)
-# 
-# network <- edgeData
