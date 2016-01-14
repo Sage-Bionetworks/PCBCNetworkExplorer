37fb985146ff5e14004c39acf41ec9fa704e54a3
kdaily <kdaily@users.noreply.github.com>
2016-01-14 12:24:44 -0800

    update server.

--- server.R
+++ server.R
@@ -10,80 +10,89 @@ library(rcytoscapejs)
 
 shinyServer(function(input, output, session) {
   
-  source("load.R")
+  session$sendCustomMessage(type="readCookie",
+                            message=list(name='org.sagebionetworks.security.user.login.token'))
   
-  comparisonReactive <- reactive({
-    tmp <- filter(edgeData, str_detect(Comparison, input$diffstate))
-    return(unique(tmp$Comparison))
-  })  
-  
-  networkReactive <- reactive({
-    comparison <- input$comparison
-    print(comparison)
-    if (comparison == "All") {
-      comparisons <- comparisonReactive()
-    }
-    else{
-      comparisons <- comparison
-    }
+  foo <- observeEvent(input$cookie, {
     
-    tmp <- filter(edgeData,
-                  # str_detect(Comparison, input$diffstate),
-                  Comparison %in% comparisons,
-                  fdr <= input$fdr)
+    synapseClient::synapseLogin(sessionToken=input$cookie)
     
-    if (!input$nontf) {
-      tmp <- filter(tmp, !(group == "nontf_mirna"))
-    }
-    else if (input$nontf & (comparison != "All"))  {
-      tmp <- tmp
-      # foo <- filter(tmp, !(group == "nontf_mirna"))
-      # putback <- filter(tmp, group == "nontf_mirna", 
-      #                   source %in% foo$source)
-      # tmp <- rbind(foo, putback)
-    }
-    else {
-      tmp <- filter(tmp, !(group == "nontf_mirna"))
-    }    
-    return(tmp)
-  })
-
-  nodeReactive <- reactive({
-    net <- networkReactive()
-    filter(nodeData, name %in% c(net$source, net$target))
-  })
-  
-  output$diffstate <- renderUI({
-    selectInput("diffstate", label='Differentiation State',
-                choices=diffStates, selected="SC", selectize=TRUE)
-  })
-  
-  output$comparison <- renderUI({
-    selectInput("comparison", "Comparison", choices=c("All", comparisonReactive()))
-  })
-  
-  output$edgeDataTable <- DT::renderDataTable({
-    net <- networkReactive()
+    source("load.R")
     
-    if (!is.null(input$connectedNodes)) {
-      net <- filter(net,
-                    source %in% input$connectedNodes | target %in% input$connectedNodes)
-    }
+    comparisonReactive <- reactive({
+      tmp <- filter(edgeData, str_detect(Comparison, input$diffstate))
+      return(unique(tmp$Comparison))
+    })  
     
-    DT::datatable(net,
-                  style='bootstrap', options=list(dom = 'tp', pageLength=5))
-  })
-  
-  output$plot <- renderRcytoscapejs({
-
-    edges <- networkReactive()
-    nodes <- nodeReactive()
-    
-    network <- createCytoscapeJsNetwork(nodes, edges)
-
-    rcytoscapejs(network$nodes, network$edges, layout=input$layout, 
-                 height='600px', highlightConnectedNodes=TRUE,
-                 boxSelectionEnabled=TRUE)
+    networkReactive <- reactive({
+      comparison <- input$comparison
+      print(comparison)
+      if (comparison == "All") {
+        comparisons <- comparisonReactive()
+      }
+      else{
+        comparisons <- comparison
+      }
+      
+      tmp <- filter(edgeData,
+                    # str_detect(Comparison, input$diffstate),
+                    Comparison %in% comparisons,
+                    fdr <= input$fdr)
+      
+      if (!input$nontf) {
+        tmp <- filter(tmp, !(group == "nontf_mirna"))
+      }
+      else if (input$nontf & (comparison != "All"))  {
+        tmp <- tmp
+        # foo <- filter(tmp, !(group == "nontf_mirna"))
+        # putback <- filter(tmp, group == "nontf_mirna", 
+        #                   source %in% foo$source)
+        # tmp <- rbind(foo, putback)
+      }
+      else {
+        tmp <- filter(tmp, !(group == "nontf_mirna"))
+      }    
+      return(tmp)
     })
-
+    
+    nodeReactive <- reactive({
+      net <- networkReactive()
+      filter(nodeData, name %in% c(net$source, net$target))
+    })
+    
+    output$diffstate <- renderUI({
+      selectInput("diffstate", label='Differentiation State',
+                  choices=diffStates, selected="SC", selectize=TRUE)
+    })
+    
+    output$comparison <- renderUI({
+      selectInput("comparison", "Comparison", choices=c("All", comparisonReactive()))
+    })
+    
+    output$edgeDataTable <- DT::renderDataTable({
+      net <- networkReactive()
+      
+      if (!is.null(input$connectedNodes)) {
+        net <- filter(net,
+                      source %in% input$connectedNodes | target %in% input$connectedNodes)
+      }
+      
+      DT::datatable(net,
+                    style='bootstrap', options=list(dom = 'tp', pageLength=5))
+    })
+    
+    output$plot <- renderRcytoscapejs({
+      
+      edges <- networkReactive()
+      nodes <- nodeReactive()
+      
+      network <- createCytoscapeJsNetwork(nodes, edges)
+      
+      rcytoscapejs(network$nodes, network$edges, layout=input$layout, 
+                   height='600px', highlightConnectedNodes=TRUE,
+                   boxSelectionEnabled=TRUE)
+    })
+    
+  })
+  
 })
